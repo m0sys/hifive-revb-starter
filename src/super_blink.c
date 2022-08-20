6f699eac593377a21798a8121ae65502686ccedf
m0sys <21181457+m0sys@users.noreply.github.com>
2022-08-20 15:19:48 -0400

    feat: add led color cycle

--- src/super_blink.c
+++ src/super_blink.c
@@ -1,24 +1,24 @@
-#include "super_blink.h"
+// #include "super_blink.h"
 #include <stdio.h>
 
-int main()
-{
-    int error = 0;
-    int led_num = 0;
-    int colors[NUM_LEDS] = { RED_LED, GREEN_LED, BLUE_LED };
-
-    setup_gpio();
-    setup_pwm();
-
-    // set_led(colors[led_num], ON);
-    // set_led(colors[led_num + 1], ON);
-    // set_led(colors[led_num + 2], ON);
-    while (!error) {
-        set_led(colors[led_num], ON);
-        delay(DELAY);
-        error = set_led(colors[led_num++], OFF);
-        delay(DELAY);
-        if (led_num >= NUM_LEDS)
-            led_num = 0;
-    }
-}
+// int main()
+// {
+//     int error = 0;
+//     int led_num = 0;
+//     int colors[NUM_LEDS] = { RED_LED, GREEN_LED, BLUE_LED };
+//
+//     setup_gpio();
+//     setup_pwm();
+//
+//     // set_led(colors[led_num], ON);
+//     // set_led(colors[led_num + 1], ON);
+//     // set_led(colors[led_num + 2], ON);
+//     while (!error) {
+//         set_led(colors[led_num], ON);
+//         delay(DELAY);
+//         error = set_led(colors[led_num++], OFF);
+//         delay(DELAY);
+//         if (led_num >= NUM_LEDS)
+//             led_num = 0;
+//     }
+// }
